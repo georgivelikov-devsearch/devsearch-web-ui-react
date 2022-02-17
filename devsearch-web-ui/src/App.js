5f9bd0990a75dd27ad0ed4a180635b35f135c3da
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-02-17 23:14:44 +0200

    First react components added with nice styling

--- devsearch-web-ui/src/App.js
+++ devsearch-web-ui/src/App.js
@@ -1,14 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Developers from "./components/Developers";
+import Projects from "./components/Projects";
+import Inbox from "./components/Inbox";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div>
-      <Header />
-      <main>
-        <h1>DEVSEARCH</h1>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/developers" element={<Developers />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/inbox" element={<Inbox />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- devsearch-web-ui/src/components/Header.js
+++ devsearch-web-ui/src/components/Header.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
+    <header className="header">
+      <div className="container container--narrow">
+        <Link className="header__logo" to="/">
+          <img src="images/logo.svg" alt="DevSearch Logo" />
+        </Link>
+        <nav className="header__nav">
+          <input type="checkbox" id="responsive-menu" />
+          <label htmlFor="responsive-menu" className="toggle-menu">
+            <span>Menu</span>
+            <div className="toggle-menu__lines"></div>
+          </label>
+          <ul className="header__menu">
+            <li className="header__menuItem">
+              <Link to="/developers">Developers</Link>
+            </li>
+            <li className="header__menuItem">
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li className="header__menuItem">
+              <Link to="/inbox">Inbox</Link>
+            </li>
+            <li className="header__menuItem">
+              <Link to="/profile">My Profile</Link>
+            </li>
+            <li className="header__menuItem">
+              <a href="login.html" className="btn btn--sub">
+                Login/Sign Up
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
